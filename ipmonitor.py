650268eaf83f4dbd2e9d0087c1d95ffe3e5b0624
alex <akg7634@gmail.com>
2014-12-20 02:23:35 -0600

    Actually added ipmanager.py to the stage this time...

--- ipmonitor.py
+++ ipmonitor.py
@@ -1,24 +1,23 @@
+#!/usr/bin/python
 import ipgetter
 import os
 import smtplib
 from email.mime.text import MIMEText
 
 def sendEmail(ip):
-	#emailFilename = "/etc/ipmonitor/email_list"
-	senderEmail = "ipmonitor-noreply@gmail.com"
-	emailFilename = "email_list"
+	emailFilename = "/etc/ipmonitor/email_list"
 	if not os.path.isfile(emailFilename):
 		print("Error - {0} not found".format(emailFilename))
 	else:
 		with open(emailFilename, "r") as emailFile:
 			for email in emailFile:
 				email = email.strip()
-				SENDMAIL = "/usr/sbin/sendmail" # sendmail location
+				SENDMAIL = "/usr/sbin/sendmail"
 				p = os.popen("%s -t" % SENDMAIL, "w")
 
 				p.write("To: {0}\n".format(email))
 				p.write("Subject: Home IP Changed: {0}\n".format(ip))
-				p.write("\n") # blank line separating headers from body
+				p.write("\n")
 				p.write("Home IP has changed to {0}\n".format(ip))
 				sts = p.close()
 				if sts != 0:
@@ -27,8 +26,7 @@ def sendEmail(ip):
 def main():
 	curIP = ipgetter.myip()
 
-	#recIPfilename = "/etc/ipmonitor/current_ip"
-	recIPfilename = "current_ip"
+	recIPfilename = "/etc/ipmonitor/current_ip"
 
 	if not os.path.isfile(recIPfilename):
 		recIPfile = open(recIPfilename, "w")
@@ -36,8 +34,7 @@ def main():
 		recIPfile.close()
 	else:
 		recIPfile = open(recIPfilename, "r")
-		#recIP = recIPfile.readline().strip()
-		recIP = ""
+		recIP = recIPfile.readline().strip()
 		recIPfile.close()
 		if recIP != curIP:
 			recIPfile = open(recIPfilename, "w")
